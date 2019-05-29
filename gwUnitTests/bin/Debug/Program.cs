f7ce40eaf939809dccca0efa5eca1148e4236a37
Brad D <braddet@gmail.com>
2019-05-28 21:55:48 -0300

    first commit

---
+++ gwUnitTests/bin/Debug/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using org.geekwisdom;
+
+namespace gwUnitTests
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string R = "";
+            string curdir = Directory.GetCurrentDirectory();
+            GWSettings mySettings = new GWSettings();
+            Console.WriteLine(curdir);
+
+            R = mySettings.GetSetting(curdir+"/example.properties", "hellomsg", "default");
+            Console.WriteLine(R);
+            R = mySettings.GetSetting(curdir+"/testnet.config", "hellomsg", "default");
+            Console.WriteLine(R);
+            Console.ReadLine();
+        }
+    }
+}
